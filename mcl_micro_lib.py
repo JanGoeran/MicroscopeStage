d2b89c4dc7cab8a919fdcf7c8fec36d3d1acd1e9
JanGoeran <71365036+JanGoeran@users.noreply.github.com>
2021-03-10 18:01:54 +1100

    Included most functions that seem useful. Ready for testing.

--- mcl_micro_lib.py
+++ mcl_micro_lib.py
@@ -3,46 +3,164 @@ import atexit
 from time import sleep
 import numpy as np
 
-class MadMicroDrive(): #good
-    def __init__(self): #good
+#Code is set up to work with one Microdrive only.
+
+# The linear actuators have a step resolution of 1.524 microns and are operated in a divide by 16 microstep mode.
+# The minimum step of the MicroStage is 95.25 nm. MicroStage stages enabled with the encoder option have a
+# measurement resolution of 50 nm.
+
+# Error Codes
+#
+# MCL_SUCCESS 0	Task has been completed successfully.
+# MCL_GENERAL_ERROR	-1 These errors generally occur due to an internal sanity check failing.
+# MCL_DEV_ERROR	-2 A problem occurred when transferring data to the Micro-Drive.
+# It is likely that the Micro-Drive will have to be power cycled to correct these errors.
+# MCL_DEV_NOT_ATTACHED -3 The Micro-Drive cannot complete the task because it is not attached.
+# MCL_USAGE_ERROR -4 Using a function from the library which the Micro-Drive does not support causes these errors.
+# MCL_DEV_NOT_READY -5 The Micro-Drive is currently completing or waiting to complete another task.
+# MCL_ARGUMENT_ERROR -6 An argument is out of range or a required pointer is equal to NULL.
+# MCL_INVALID_AXIS -7 Attempting an operation on an axis that does not exist in the Micro-Drive.
+# MCL_INVALID_HANDLE -8 The handle is not valid. Or at least is not valid in this instance of the DLL.
+
+class MadMicroDrive:
+
+    def __init__(self):
         # provide valid path to Madlib.dll. Madlib.h and Madlib.lib should also be in the same folder
-        path_to_dll = 'C:/Program Files/Mad City Labs/MicroDrive/MicroDrive.dll' #good
-        self.madlib = cdll.LoadLibrary(path_to_dll) #good
-        self.handler = self.mcl_start() #good
-        atexit.register(self.mcl_close) #good
-        self.status = c_ushort
+        path_to_dll = 'C:/Program Files/Mad City Labs/MicroDrive/MicroDrive.dll'
+        self.madlib = cdll.LoadLibrary(path_to_dll)
+        self.handler = self.mcl_start()
+        atexit.register(self.mcl_close)
+
+        #position
+        self.currentPositionByMove = np.array([0,0,0])
+        self.currentPositionByRead = np.array([0,0,0])
+
+        #pointers
+        self.encoderRes = c_double()
+        self.encoderRes_pointer = pointer(self.encoderRes)
+        self.stepSize = c_double()
+        self.stepSize_pointer = pointer(self.stepSize)
+        self.maxVelocity = c_double()
+        self.maxVelocity_pointer = pointer(self.maxVelocity)
+        self.maxVelocityTwoAxis = c_double()
+        self.maxVelocityTwoAxis_pointer = pointer(self.maxVelocityTwoAxis)
+        self.maxVelocityThreeAxis = c_double()
+        self.maxVelocityThreeAxis_pointer = pointer(self.maxVelocityThreeAxis)
+        self.minVelocity = c_double()
+        self.minVelocity_pointer = pointer(self.minVelocity)
+
+        self.firmwareVersion = c_short()
+        self.firmwareVersion_pointer = pointer(self.firmwareVersion)
+        self.firmwareProfile = c_short()
+        self.firmwareProfile_pointer = pointer(self.firmwareProfile)
+
+        self.encoderBM = c_ubyte()
+        self.encoderBM_pointer = pointer(self.encoderBM)
+
+        self.axisBM = c_ubyte()
+        self.axisBM_pointer = pointer(self.axisBM)
+
+        self.status = c_ushort()
         self.status_pointer = pointer(self.status)
-        self.isMoving = c_int
+        self.isMoving = c_int()
         self.isMoving_pointer = pointer(self.isMoving)
-        self.xSteps = c_int
+        self.xSteps = c_int()
         self.xSteps_pointer = pointer(self.xSteps)
-        self.ySteps = c_int
+        self.ySteps = c_int()
         self.ySteps_pointer = pointer(self.ySteps)
 
+        #for encoder
+        self.e1 = c_double()
+        self.e1_pointer = pointer(self.e1)
+        self.e2 = c_double()
+        self.e2_pointer = pointer(self.e2)
+        self.e3 = c_double()
+        self.e3_pointer = pointer(self.e3)
+        self.e4 = c_double()
+        self.e4_pointer = pointer(self.e4)
+
+    # int MCL_InitHandle()
     def mcl_start(self):
         """
-        Requests control of a single Mad City Labs Nano-Drive.
-        Return Value:
-            Returns a valid handle or returns 0 to indicate failure.
+        Requests control of a single Mad City Labs Micro-Drive. Is called in initialize
         """
-        mcl_init_handle = self.madlib['MCL_InitHandle']  #good
+        mcl_init_handle = self.madlib['MCL_InitHandle']
 
-        mcl_init_handle.restype = c_int #good
-        handler = mcl_init_handle() #good
-        if (handler == 0): #good
-            print("MCL init error") #good
-            return -1 #good
-        return handler #good
+        mcl_init_handle.restype = c_int
+        handler = mcl_init_handle()
+        if (handler == 0):
+            print("MCL init error")
+            return -1
+        return handler
+
+    # int MCL_MDInformation(
+    #     double * encoderResolution,
+    #     double * stepSize,
+    #     double * maxVelocity,
+    #     double * maxVelocityTwoAxis,
+    #     double * maxVelocityThreeAxis,
+    #     double * minVelocity,
+    #     int handle)
+    def get_information(self):
+        """Gather Information about the resolution and speed of the Micro-Drive."""
+
+        mcl_get_info = self.madlib['MCL_MDInformation']
+        mcl_get_info.restype = c_int
+        mcl_get_info(self.encoderRes_pointer, self.stepSize_pointer, self.maxVelocity_pointer,
+                     self.maxVelocityTwoAxis_pointer, self.maxVelocityThreeAxis_pointer,
+                     self.minVelocity_pointer, c_int(self.handler))
+        info = [self.encoderRes.value, self.stepSize.value, self.maxVelocity.value,
+                     self.maxVelocityTwoAxis.value, self.maxVelocityThreeAxis.value,
+                     self.minVelocity.value]
+        forprint = ['Encoder res (um): ', 'step: ', 'max velocity M1: ', 'max velocity M2: ', 'max velocity M3: ',
+                    'min velocity: ']
+
+        for i in range(len(info)):
+            print(forprint[i] + str(info[i]))
+
+        return info
+
+    #int MCL_MDEncodersPresent(unsigned char* encoderBitmap, int handle)
+    def encoder_present(self):
+        """Determine which encoders are present in the Micro-Drive."""
+
+        mcl_encoder_present = self.madlib['MCL_MDEncodersPresent']
+        mcl_encoder_present.restype = c_int
+        mcl_encoder_present(self.encoderBM_pointer, c_int(self.handler))
+
+        l = [d for d in str(self.encoderBM.value)]
+        l.reverse()
+        Encoders = ['Encoder 1: ', 'Encoder 2: ', 'Encoder 3: ', 'Encoder 4: ']
+        for i in range(4):
+            print(Encoders[i] + l[i])
+
+        return self.encoderBM.value
 
     #int MCL_MDStatus(unsigned short* status, int handle)
     def get_status(self):
+        """Reads the limit switches."""
+
         mcl_get_status = self.madlib['MCL_MDStatus']
         mcl_get_status.restype = c_int
         mcl_get_status(self.status_pointer, c_int(self.handler))
+
+        l = [d for d in str(self.status.value)]
+        l.reverse()
+
+        status = ['M1 Reverse Limit Switch: ', 'M1 Forward Limit Switch: ', 'M2 Reverse Limit Switch: ',
+                  'M2 Forward Limit Switch: ', 'M3 Reverse Limit Switch: ', 'M3 Forward Limit Switch: ',
+                  'Success/Failure!: ']
+
+        for i in range(7):
+            print(status[i] + l[i])
+
         return self.status.value
+
 
     #int MCL_MDStop(unsigned short* status, int handle)
     def stop_move(self):
+        """Stops the stage from moving."""
+
         mcl_get_status = self.madlib['MCL_MDStop']
         mcl_get_status.restype = c_int
         mcl_get_status(self.status_pointer, c_int(self.handler))
@@ -50,6 +168,10 @@ class MadMicroDrive(): #good
 
     #int MCL_MicroDriveMoveStatus(int * isMoving, int handle)
     def is_moving(self):
+        """
+        Queries the device to see if it is moving. This function should be called prior
+        to reading the encoders as the encoders should not be read when the stage is in motion.
+        """
         mcl_is_moving = self.madlib['MCL_MicroDriveMoveStatus']
         mcl_is_moving.restype = c_int
         mcl_is_moving(self.isMoving_pointer, c_int(self.handler))
@@ -57,6 +179,8 @@ class MadMicroDrive(): #good
 
     #int MCL_MicroDriveWait(int handle)
     def wait(self):
+        """Waits long enough for the previously commanded move to finish."""
+
         print('start wait')
         mcl_wait = self.madlib['MCL_MicroDriveWait']
         mcl_wait.restype = c_int
@@ -66,9 +190,17 @@ class MadMicroDrive(): #good
     #int MCL_MDMoveThreeAxesR(int axis1, double velocity1, double distance1, int rounding1, int axis2, double
     # velocity2, double distance2, int rounding2, int axis3, double velocity3, double distance3, int rounding3,
     # int handle)
-
-
     def move_R(self, rel_coordinates, velocity = 0.01, rounding = 1):
+        """
+        Standard movement function. Acceleration and deceleration ramps are generated for the specified motion.
+        In some cases when taking smaller steps the velocity parameter may be coerced to its maximum achievable value.
+        The maximum and minimum velocities can be found using MCL_MDInformation.
+        The maximum velocity differs depending on how many axes are commaned to move.
+        :param rel_coordinates: list with three elements with relative movement in [mm]. Example [0.2,0.1,0]
+        :param velocity: velocity of movement.
+        :param rounding: 0 - Nearest microstep. 1 - Nearest full step. 2 - Nearest half step.
+        """
+
         mcl_move_three_axesR = self.madlib['MCL_MDMoveThreeAxesR']
         mcl_move_three_axesR.restype = c_int
         mcl_move_three_axesR(c_int(1), c_double(velocity), c_double(rel_coordinates[0]), c_int(rounding),
@@ -76,9 +208,36 @@ class MadMicroDrive(): #good
                              c_int(3), c_double(velocity), c_double(rel_coordinates[2]), c_int(rounding),
                              c_int(self.handler))
         self.wait()
+        self.currentPositionByMove = self.currentPositionByMove + rel_coordinates
+        self.currentPositionByRead = self.currentPositionByRead + np.array(self.read_encoders()+[0])
+
+    #int MCL_MDResetEncoders(unsigned short* status, int handle)
+    def set_origin(self):
+        """Resetting an encoder makes the current position of the microstage the zero position of the encoder.
+         Use  MCL_MDResetEncoders to reset all encoders or  MCL_MDResetEncoder to reset a specifc encoder."""
+
+        mcl_reset_encoders = self.madlib['MCL_MDResetEncoders']
+        mcl_reset_encoders.restype = c_int
+        mcl_reset_encoders(self.status_pointer, c_int(self.handler))
+        self.currentPositionByRead = [0, 0, 0]
+        self.currentPositionByMove = [0, 0, 0]
+
+        print('set origin at current position')
+
+    #int MCL_MDReadEncoders(double* e1, double* e2, double *e3, double *e4, int handle)
+    def read_encoders(self):
+        """Reads all encoders"""
+
+        mcl_read_encoders = self.madlib['MCL_MDReadEncoders']
+        mcl_read_encoders.restype = c_int
+        mcl_read_encoders(self.e1_pointer, self.e2_pointer, self.e3_pointer, self.e4_pointer, c_int(self.handler))
+        print('position: ' + str([self.e1.value, self.e2.value]))
+        return [self.e1.value, self.e2.value]
 
     #int MCL_MDCurrentPositionM(unsigned int axis, int *microSteps, int handle)
     def read_position(self, axis):
+        """Reads the number of microsteps taken since the beginning of the program."""
+
         mcl_current_position = self.madlib['MCL_MDCurrentPositionM']
         mcl_current_position.restype = c_int
         if axis == 1:
@@ -90,69 +249,57 @@ class MadMicroDrive(): #good
         else:
             print('axis needs to be 1 or 2')
 
-
-
-    def mcl_read(self, axis_number): #this needs serious revision for the microcontroller
-        """
-        Read the current position of the specified axis.
-
-        Parameters:
-            axis [IN] Which axis to move. (X=1,Y=2,Z=3,AUX=4)
-            handle [IN] Specifies which Nano-Drive to communicate with.
-        Return Value:
-            Returns a position value or the appropriate error code.
-        """
-        mcl_single_read_n = self.madlib['MCL_SingleReadN'] #int MCL_MDCurrentPositionM(unsigned int axis, int *microSteps, int handle)?
-        mcl_single_read_n.restype = c_double
-        return mcl_single_read_n(c_uint(axis_number), c_int(self.handler))
-
-    def mcl_write(self, position, axis_number):
-        """
-        Commands the Nano-Drive to move the specified axis to a position.
-
-        Parameters:
-            position [IN] Commanded position in microns.
-            axis [IN] Which axis to move. (X=1,Y=2,Z=3,AUX=4)
-            handle [IN] Specifies which Nano-Drive to communicate with.
-        Return Value:
-            Returns MCL_SUCCESS or the appropriate error code.
-        """
-        mcl_single_write_n = self.madlib['MCL_SingleWriteN']
-        mcl_single_write_n.restype = c_int
-        error_code = mcl_single_write_n(c_double(position), c_uint(axis_number), c_int(self.handler))
-
-        if (error_code != 0):
-            print("MCL write error = ", error_code)
-        return error_code
-
-    def goxy(self, x_position, y_position):
-        self.mcl_write(x_position, 1)
-        self.mcl_write(y_position, 2)
-
-    def goz(self, z_position):
-        self.mcl_write(z_position, 3)
-
-    def get_position(self):
-        return self.mcl_read(1), self.mcl_read(2), self.mcl_read(3)
-
+    #void MCL_ReleaseAllHandles()
     def mcl_close(self):
         """
-        Releases control of all Nano-Drives controlled by this instance of the DLL.
+        Releases control of all Micro-Drives controlled by this instance of the DLL.
         """
         mcl_release_all = self.madlib['MCL_ReleaseAllHandles']
         mcl_release_all()
+
+    #int  MCL_GetFirmwareVersion(short *version, short *profile, int handle)
+    def get_firmware(self):
+        """Gives access to the Firmware version and profile information."""
+        mcl_get_firmware_version = self.madlib['MCL_GetFirmwareVersion']
+        mcl_get_firmware_version.restype = c_int
+        mcl_get_firmware_version(self.firmwareVersion_pointer, self.firmwareProfile_pointer, c_int(self.handler))
+        print('version is: ' + str(self.firmwareVersion.value))
+        print('profile is: ' + str(self.firmwareProfile.value))
+        return [self.firmwareVersion.value, self.firmwareProfile.value]
+
+    #int MCL_GetSerialNumber(int handle)
+    def get_serial(self):
+        """Returns the serial number of the Micro-Drive.
+        This information can be useful if  you need support for your device
+        or if you are attempting to tell the difference between two similar Micro-Drives.
+        """
+        mcl_get_serial = self.madlib['MCL_GetSerialNumber']
+        mcl_get_serial.restype = c_int
+        serial = mcl_get_serial(c_int(self.handler))
+        print('serial is: ' + str(serial))
+        return serial
+
+    #int MCL_GetAxisInfo(unsigned char *axis_bitmap, int handle)
+    def get_axis_info(self):
+        """Allows the program to query which axes are available."""
+        mcl_get_axis_info = self.madlib['MCL_GetAxisInfo']
+        mcl_get_axis_info.restype = c_int
+        mcl_get_axis_info(self.axisBM_pointer, c_int(self.handler))
+
+        l = [d for d in str(self.axisBM.value)]
+        l.reverse()
+
+        status = ['M1 valid: ', 'M2 valid: ', 'M3 valid: ', 'M4 valid: ', 'M5 valid: ', 'M6 valid: ']
+
+        for i in range(len(status)):
+            print(status[i] + l[i])
+
+        return self.status.value
 
 
 if __name__ == "__main__":
 
     Micro1 = MadMicroDrive()
-
-
-
     rel_move = [0,0,0]
-
-
-
-
     Micro1.mcl_close()
 
